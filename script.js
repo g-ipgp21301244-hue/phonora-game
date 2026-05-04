5827866baeb39a44bfc879a00206906a4f619954
g-ipgp21301244-hue <g-ipgp21301244@moe-dl.edu.my>
2026-05-04 10:11:19 +0800

    Update script.js

--- script.js
+++ script.js
@@ -488,3 +488,6 @@ window.goToRoles = function () {
     document.getElementById("menu").classList.add("hidden");
     document.getElementById("roles").classList.remove("hidden");
 };
+function goToRoles() {
+    alert("CLICK WORKED");
+}
